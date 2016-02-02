5592ec94dd43ddd724f24b06ac075ce0237b9599
Zach Silveira <zackify@gmail.com>
2016-02-02 02:06:38 -0500

    first push

---
+++ routes/home.js
@@ -0,0 +1,9 @@
+import { Route } from '../../reup'
+
+export default class Home extends Route {
+  static component = 'home';
+
+  actions() {
+    return 'blah'
+  }
+}
---
+++ views/home.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default props => (
+  <div>Hiiiii there im loaded</div>
+)
